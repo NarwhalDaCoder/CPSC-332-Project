e66902aa8cd63d065727c698023bee1b7dc56d59
ChrisMelgar <cmelgarmorales@csu.fullerton.edu>
2022-05-06 18:41:55 -0700

    moved navbar to seperate php file, to add to page with include instead

--- searchResults.php
+++ searchResults.php
@@ -132,43 +132,7 @@ if ($stmt = mysqli_prepare($mysqli, $sql))
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
       <div class = "wrapper">
       <div class="container d-grid gap-2">
-         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-               <a class="navbar-brand" href="#">Lindeed</a>
-               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                     </li>
-                     <li class="nav-item dropdown <?php echo (!isset($_SESSION["employer"])) ? 'd-none' : ''; ?>">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Employer Tools
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                           <li><a class="dropdown-item" href="createJobPost.php">Create Jobpost</a></li>
-                           <li><a class="dropdown-item" href="viewEmployerPosts.php">View Jobposts</a></li>
-                        </ul>
-                     </li>
-                     <li class="nav-item dropdown <?php echo (!isset($_SESSION["employee"])) ? 'd-none' : ''; ?>">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Employee Tools
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                           <li><a class="dropdown-item" href="#">My Jobpost</a></li>
-                           <li><a class="dropdown-item" href="#">My Applications</a></li>
-                           <li><a class="dropdown-item" href="searchResults.php">Search Jobposts</a></li>
-                        </ul>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Logout</a>
-                     </li>
-                  </ul>
-               </div>
-            </div>
-         </nav>
+         <?php include 'navBar.php' ?>
          <br>
          <?php  foreach($arr as $key => $value)
             { ?>
